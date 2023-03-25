513a4fcdf78c464a24ca8b44b4fad03d55cb029e
FloresTM <seb.flores2002@gmail.com>
2023-03-24 20:06:03 -0600

    menu y modal menu top

--- client/src/pages/Home/TopMenu/TopMenuModal/TopMenuModal.tsx
+++ client/src/pages/Home/TopMenu/TopMenuModal/TopMenuModal.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import styles from "./TopMenuModal.module.css";
 
 function TopMenuModal() {
-	return <div>TopMenuModal</div>;
+	return (
+		<div className={styles.page}>
+			<div className={styles.individual}>
+				<img
+					className={styles.icon}
+					src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png"
+				/>
+				<p className={styles.p}>Perfil</p>
+			</div>
+			<div className={styles.border}></div>
+			<div className={styles.individual}>
+				<img
+					className={styles.icon}
+					src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png"
+				/>
+				<p className={styles.p}>Cerrar Sesion</p>
+			</div>
+		</div>
+	);
 }
 
 export default TopMenuModal;
