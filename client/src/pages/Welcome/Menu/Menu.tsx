d6e1da92241e84b16f8318cd57bd2fe0be1b411b
FloresTM <seb.flores2002@gmail.com>
2023-03-26 23:03:49 -0600

    sobre nosotros responsive, burger menu 1/2

--- client/src/pages/Welcome/Menu/Menu.tsx
+++ client/src/pages/Welcome/Menu/Menu.tsx
@@ -6,6 +6,15 @@ function Menu() {
 	const [activeIndex, setActiveIndex] = useState(-1);
 	const [isBurgerActive, setIsBurgerActive] = useState(false);
 
+	const handleBurgerClick = () => {
+		setIsBurgerActive(!isBurgerActive);
+	};
+
+	const handleLinkClick = (index: number) => {
+		setActiveIndex(index);
+		setIsBurgerActive(false);
+	};
+
 	return (
 		<div className={styles.page}>
 			<div className={styles.start}>
@@ -51,6 +60,55 @@ function Menu() {
 					<p className={styles.individual}>Contáctanos</p>
 				</Link>
 			</div>
+			<div className={styles.burger} onClick={handleBurgerClick}>
+				≡
+			</div>
+			{isBurgerActive && (
+				<div className={styles.mobileMenu}>
+					<Link
+						to="/"
+						onClick={() => handleLinkClick(0)}
+						className={`${styles.link} ${
+							activeIndex === 0 ? styles.active : ""
+						}`}
+					>
+						Inicio
+					</Link>
+					<Link
+						to="/sobre-nosotros"
+						onClick={() => {
+							setActiveIndex(1);
+						}}
+						className={`${styles.link} ${
+							activeIndex === 1 ? styles.active : ""
+						}`}
+					>
+						<p className={styles.individual}>Sobre Nosotros</p>
+					</Link>
+					<Link
+						onClick={() => {
+							setActiveIndex(2);
+						}}
+						className={`${styles.link} ${
+							activeIndex === 2 ? styles.active : ""
+						}`}
+						to="/faq"
+					>
+						<p className={styles.individual}>FAQ</p>
+					</Link>
+					<Link
+						onClick={() => {
+							setActiveIndex(3);
+						}}
+						className={`${styles.link} ${
+							activeIndex === 3 ? styles.active : ""
+						}`}
+						to="/contact"
+					>
+						<p className={styles.individual}>Contáctanos</p>
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 }
