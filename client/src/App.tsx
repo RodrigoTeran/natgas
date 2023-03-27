62b2da581d3f3ff52353345f2ad31f0f0a66097a
FloresTM <seb.flores2002@gmail.com>
2023-03-27 12:23:49 -0600

    push de login y registor mas guapo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
+import InfoRegistro from "./pages/InfoRegistro/InfoRegistro";
 import LogIn from "./pages/LogIn/LogIn";
 import Measurements from "./pages/Measurements/Measurements";
+import Register from "./pages/Register/Register";
 import AboutUs from "./pages/Welcome/AboutUs/AboutUs";
 import ContactUs from "./pages/Welcome/ContactUs/ContactUs";
 import FAQ from "./pages/Welcome/FAQ/FAQ";
@@ -21,6 +23,10 @@ function App() {
 				<Route path="/faq" element={<FAQ />}></Route>
 
 				<Route path="/sobre-nosotros" element={<AboutUs />}></Route>
+
+				<Route path="/registro" element={<Register />}></Route>
+
+				<Route path="/info-registro" element={<InfoRegistro />}></Route>
 
 				<Route path="/home" element={<Home />}></Route>
 
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,9 +1,8 @@
-import styles from "./LogIn.module.css";
-import Btn from "./Btns/Btn";
-import { logInRoute } from "../../routes/auth/auth.routes";
+import styles from "./Register.module.css";
+import Btn from "../LogIn/Btns/Btn";
 import { Link } from "react-router-dom";
 
-function LogIn() {
+function Register() {
 	return (
 		<div className={styles.page}>
 			<div className={styles.top}>
@@ -11,17 +10,18 @@ function LogIn() {
 					className={styles.icon}
 					src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png"
 				/>
-				<h1 className={styles.title}>Hola otra vez!</h1>
+				<h1 className={styles.title}>Bounjourno!</h1>
 				<p className={styles.p}>
 					Lorem ipsum dolor sit amet <br />
 					consectetur adipisicing elit
 				</p>
-				<Btn color="#1F87FC" provider="Google">
-					<div className={styles.content}>
-						Inciar sesión con <span className={styles.bold}>Google</span>
-					</div>
-				</Btn>
-				<a href={logInRoute}>Iniciar sesión</a>
+				<Link className={styles.underlineless} to="/info-registro">
+					<Btn color="#1F87FC" provider="Google">
+						<div className={styles.content}>
+							Inciar sesión con <span className={styles.bold}>Google</span>
+						</div>
+					</Btn>
+				</Link>
 				<Btn color="#3C5998" provider="Facebook">
 					<div className={styles.content}>
 						Inciar sesión con <span className={styles.bold}>Facebook</span>
@@ -30,14 +30,12 @@ function LogIn() {
 			</div>
 			<div className={styles.bottom}>
 				<p className={styles.register_p}>
-					Don’t have an account yet?{" "}
-					<Link className={styles.underlineless} to="/registro">
-						<span className={styles.register_span}>Register</span>
-					</Link>
+					Don't have an account yet?{" "}
+					<span className={styles.register_span}>Register</span>
 				</p>
 			</div>
 		</div>
 	);
 }
 
-export default LogIn;
+export default Register;
