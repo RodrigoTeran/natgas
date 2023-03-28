5fc4da3a635032616142cf15e000d93b318cc4ea
ArmandoRosasB <armando.rosas133@gmail.com>
2023-03-28 00:55:51 -0600

    fix: queries and front

--- maquetacion_progreso/queries.sql
+++ maquetacion_progreso/queries.sql
@@ -5,3 +5,11 @@ SELECT measurement
 FROM ?, client
 WHERE client.id = ?.clientid
 AND client.id = ?;
+
+-- Consultar medida corporal con fecha
+
+SELECT measurement
+FROM ?, client
+WHERE client.id = ?.clientid
+AND client.id = ?
+AND created_at BETWEEN ? AND ?;
