3ea0e56ab65d3f087ba4c53a124c3d98eb8cf1a2
DHurtado714-itesm <a01707774@itesm.mx>
2023-03-28 22:57:12 -0600

    query for find by date, title or content

--- Bitacora/src/models/Bitacora/bitacora.model.ts
+++ Bitacora/src/models/Bitacora/bitacora.model.ts
@@ -14,25 +14,40 @@ class Bitacora {
 		this.title = title;
 		this.content = content;
 	}
-    // Find entry by user and week date
+	// Find entry by user and week date
 	static async findByUser(clientId: string, date: Date): Promise<IBitacora[]> {
 		const [rows] = await pool.execute(
 			`SELECT aDate, title, content FROM journalEntry WHERE clientId = ? AND aDate BETWEEN ? AND ?;`,
 			[clientId, date, date.setDate(date.getDate() + 6)]
 		);
-        console.log(rows)
-        return rows;
+		console.log(rows);
+		return rows;
 	}
-    // Write a new entry to the database
-	async mewEntry(clientId: string): Promise<IBitacora | null> {
 
+	// Find entry by content, date, or title
+	static async findByParam(
+		clientId: string,
+		aDate: Date,
+		title: string,
+		content: string
+	): Promise<IBitacora[]> {
+		const [rows] = await pool.execute(
+			`SELECT aDate, title, content FROM journalEntry WHERE clientId = ? AND (aDate = ? OR title = ? OR content = ?);`,
+			[clientId, aDate, title, content]
+		);
+		console.log(rows);
+		return rows;
+	}
+
+	// Write a new entry to the database
+	async mewEntry(clientId: string): Promise<IBitacora | null> {
 		await pool.execute(
 			`INSERT INTO journalEntry(id, aDay, title, content, clientId) VALUES
             (?, ?, ?, ?, ?);`,
 			[this.id, this.aDate, this.title, this.content, clientId]
 		);
 
-        if(this.title.length == 0 || this.content.length == 0) return null;
+		if (this.title.length == 0 || this.content.length == 0) return null;
 	}
 }
 
