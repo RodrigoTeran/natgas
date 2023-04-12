cec2b36d775efb4bdd9127a6997ab04440cc4bb1
FloresTM <seb.flores2002@gmail.com>
2023-04-11 19:29:00 -0600

    inputs para info de perfil

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -3,8 +3,90 @@ import Dashboard from "../../layouts/Dashboard/Dashboard";
 import styles from "./Profile.module.css";
 import arrow from "../Bitacora/icons/left-arrow.png";
 import placeholder from "../Workouts/images/placeholder-image.jpg";
+import { useState } from "react";
 
 function Profile() {
+	const [currentlyEditing, setCurrentlyEditing] = useState<string | null>(null);
+	const [nombres, setNombres] = useState<string>("Sebastian Armando");
+	const [apellidos, setApellidos] = useState<string>("Flores Lemus");
+	const [weight, setWeight] = useState<number>(60);
+	const [height, setHeight] = useState<number>(1.7);
+	const [birthDate, setBirthDate] = useState<string>("19 / 02 / 2023");
+	const [goal, setGoal] = useState<string>("Bajar de peso");
+	const [level, setLevel] = useState<string>("Caminar diario");
+
+	const handleEdit = (field: string) => {
+		setCurrentlyEditing(field);
+	};
+
+	const handleCancel = () => {
+		setCurrentlyEditing(null);
+	};
+
+	const handleSave = () => {
+		// proximos fetches
+		setCurrentlyEditing(null);
+	};
+
+	const renderEditableField = (
+		fieldKey: string,
+		fieldValue: string | number,
+		label: string
+	) => {
+		return (
+			<div className={styles.cuenta_body_individual}>
+				<div className={styles.titulo_botones_row}>
+					<h5 className={styles.h5}>{label}:</h5>
+					{currentlyEditing === fieldKey && (
+						<div className={styles.botones_input}>
+							<button className={styles.button_cancelar} onClick={handleCancel}>
+								Cancelar
+							</button>
+							<button className={styles.button_guardar} onClick={handleSave}>
+								Guardar
+							</button>
+						</div>
+					)}
+				</div>
+
+				<div className={styles.cuenta_body_row}>
+					{currentlyEditing === fieldKey ? (
+						<input
+							type="text"
+							value={fieldValue}
+							className={`${styles.cuenta_body_row_value} ${
+								currentlyEditing === fieldKey ? styles.active : ""
+							}`}
+							onChange={(e) => {
+								if (fieldKey === "nombres") {
+									setNombres(e.target.value);
+								} else if (fieldKey === "apellidos") {
+									setApellidos(e.target.value);
+								} else if (fieldKey === "weight") {
+									setWeight(parseFloat(e.target.value));
+								} else if (fieldKey === "height") {
+									setHeight(parseFloat(e.target.value));
+								} else if (fieldKey === "birthDate") {
+									setBirthDate(e.target.value);
+								} else if (fieldKey === "goal") {
+									setGoal(e.target.value);
+								} else if (fieldKey === "level") {
+									setLevel(e.target.value);
+								}
+							}}
+						/>
+					) : (
+						<p className={styles.cuenta_body_row_value}>{fieldValue}</p>
+					)}
+					<img
+						src={arrow}
+						className={styles.arrow_img}
+						onClick={() => handleEdit(fieldKey)}
+					/>
+				</div>
+			</div>
+		);
+	};
 	return (
 		<Dashboard>
 			<div className={styles.page}>
@@ -28,43 +110,15 @@ function Profile() {
 							</div>
 						</div>
 						<div className={styles.cuenta_body}>
-							<div className={styles.cuenta_body_individual}>
-								<h5 className={styles.h5}>Nombres:</h5>
-								<div className={styles.cuenta_body_row}>
-									<p className={styles.cuenta_body_row_value}>
-										Sebastian Armando
-									</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
-							<div className={styles.cuenta_body_individual}>
-								<h5 className={styles.h5}>Apellidos:</h5>
-								<div className={styles.cuenta_body_row}>
-									<p className={styles.cuenta_body_row_value}>Flores Lemus</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
-							<div className={styles.cuenta_body_individual}>
-								<h5 className={styles.h5}>Peso:</h5>
-								<div className={styles.cuenta_body_row}>
-									<p className={styles.cuenta_body_row_value}>65 kg</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
-							<div className={styles.cuenta_body_individual}>
-								<h5 className={styles.h5}>Altura:</h5>
-								<div className={styles.cuenta_body_row}>
-									<p className={styles.cuenta_body_row_value}>1.73 metros</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
-							<div className={styles.cuenta_body_individual}>
-								<h5 className={styles.h5}>Fecha de Nacimiento:</h5>
-								<div className={styles.cuenta_body_row}>
-									<p className={styles.cuenta_body_row_value}>19 / 02 / 2023</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
+							{renderEditableField("nombres", nombres, "Nombres")}
+							{renderEditableField("apellidos", apellidos, "Apellidos")}
+							{renderEditableField("weight", `${weight} kg`, "Peso")}
+							{renderEditableField("height", `${height} metros`, "Altura")}
+							{renderEditableField(
+								"birthDate",
+								birthDate,
+								"Fecha de Nacimiento"
+							)}
 						</div>
 					</div>
 					<div className={styles.entrenamiento}>
@@ -76,24 +130,8 @@ function Profile() {
 							</div>
 						</div>
 						<div className={styles.entrenamiento_body}>
-							<div className={styles.entrenamiento_body_individual}>
-								<h5 className={styles.h5}>Meta:</h5>
-								<div className={styles.entrenamiento_body_row}>
-									<p className={styles.entrenamiento_body_row_value}>
-										Bajar de peso
-									</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
-							<div className={styles.entrenamiento_body_individual}>
-								<h5 className={styles.h5}>Nivel:</h5>
-								<div className={styles.entrenamiento_body_row}>
-									<p className={styles.entrenamiento_body_row_value}>
-										Caminar Diario
-									</p>
-									<img src={arrow} className={styles.arrow_img} />
-								</div>
-							</div>
+							{renderEditableField("goal", goal, "Meta")}
+							{renderEditableField("level", level, "Nivel")}
 						</div>
 					</div>
 					<div className={styles.ayuda}>
