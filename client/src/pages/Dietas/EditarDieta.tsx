8b922b5da797ce46fccc463f3d824a1730f8bfa1
ArmandoRosasB <armando.rosas133@gmail.com>
2023-04-24 09:42:02 -0600

    fix: dietas

--- client/src/pages/Dietas/EditarDieta.tsx
+++ client/src/pages/Dietas/EditarDieta.tsx
@@ -283,72 +283,72 @@ export const EditarDieta = ({
                         <div className={styles.micros_info}>
                             <div className={styles.micros_col}>
                                 <label htmlFor="real_fibra">Fibra (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="" id="real_" name="real_" required onChange={(e) => {setNutrients("Fibra", e.target.value.toString(), 0)}}  value={micros["Fibra"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="" id="real_" name="real_" required onChange={(e) => {setNutrients("Fibra", e.target.value.toString(), 0)}}  value={micros["Fibra"]}/> <br/>
 
                                 <label htmlFor="real_ceniza">Ceniza (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Ceniza" id="real_ceniza" name="real_ceniza" required onChange={(e) => {setNutrients("Ceniza", e.target.value.toString(), 0)}}  value={micros["Ceniza"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Ceniza" id="real_ceniza" name="real_ceniza" required onChange={(e) => {setNutrients("Ceniza", e.target.value.toString(), 0)}}  value={micros["Ceniza"]}/> <br/>
 
                                 <label htmlFor="real_calcio">Calcio (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Calcio" id="real_calcio" name="real_calcio" required  onChange={(e) => {setNutrients("Calcio", e.target.value.toString(), 0)}}  value={micros["Calcio"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Calcio" id="real_calcio" name="real_calcio" required  onChange={(e) => {setNutrients("Calcio", e.target.value.toString(), 0)}}  value={micros["Calcio"]}/> <br/>
 
                                 <label htmlFor="real_fósforo">Fósforo (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Fósforo" id="real_fósforo" name="real_fósforo" required  onChange={(e) => {setNutrients("Fósforo", e.target.value.toString(), 0)}}  value={micros["Fósforo"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Fósforo" id="real_fósforo" name="real_fósforo" required  onChange={(e) => {setNutrients("Fósforo", e.target.value.toString(), 0)}}  value={micros["Fósforo"]}/> <br/>
                                 
                                 <label htmlFor="real_hierro">Hierro (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Hierro" id="real_hierro" name="real_hierro" required  onChange={(e) => {setNutrients("Hierro", e.target.value.toString(), 0)}}  value={micros["Hierro"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Hierro" id="real_hierro" name="real_hierro" required  onChange={(e) => {setNutrients("Hierro", e.target.value.toString(), 0)}}  value={micros["Hierro"]}/> <br/>
 
                                 <label htmlFor="real_tiamina">Tiamina (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Tiamina" id="real_tiamina" name="real_tiamina" required  onChange={(e) => {setNutrients("Tiamina", e.target.value.toString(), 0)}}  value={micros["Tiamina"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Tiamina" id="real_tiamina" name="real_tiamina" required  onChange={(e) => {setNutrients("Tiamina", e.target.value.toString(), 0)}}  value={micros["Tiamina"]}/> <br/>
 
                                 <label htmlFor="real_riboflavina">Riboflavina (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Riboflavina" id="real_riboflavina" name="real_riboflavina" required  onChange={(e) => {setNutrients("Riboflavina", e.target.value.toString(), 0)}}   value={micros["Riboflavina"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Riboflavina" id="real_riboflavina" name="real_riboflavina" required  onChange={(e) => {setNutrients("Riboflavina", e.target.value.toString(), 0)}}   value={micros["Riboflavina"]}/> <br/>
 
                                 <label htmlFor="real_niacina">Niacina (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Niacina" id="real_niacina" name="real_niacina" required  onChange={(e) => {setNutrients("Niacina", e.target.value.toString(), 0)}}   value={micros["Niacina"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Niacina" id="real_niacina" name="real_niacina" required  onChange={(e) => {setNutrients("Niacina", e.target.value.toString(), 0)}}   value={micros["Niacina"]}/> <br/>
 
                                 <label htmlFor="real_vitamina C">Vitamina C (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Vitamina C" id="real_vitamina C" name="real_vitamina C" required  onChange={(e) => {setNutrients("Vitamina C", e.target.value.toString(), 0)}}   value={micros["Vitamina C"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Vitamina C" id="real_vitamina C" name="real_vitamina C" required  onChange={(e) => {setNutrients("Vitamina C", e.target.value.toString(), 0)}}   value={micros["Vitamina C"]}/> <br/>
                                 
                                 <label htmlFor="real_vitamina A">Vitamina A (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Vitamina A" id="real_vitamina A" name="real_vitamina A" required onChange={(e) => {setNutrients("Vitamina A", e.target.value.toString(), 0)}}  value={micros["Vitamina A"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Vitamina A" id="real_vitamina A" name="real_vitamina A" required onChange={(e) => {setNutrients("Vitamina A", e.target.value.toString(), 0)}}  value={micros["Vitamina A"]}/> <br/>
 
                                 <label htmlFor="real_acGrasosMono">Ac grasos mono-in (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos mono-in" id="real_acGrasosMono" name="real_acGrasosMono" required onChange={(e) => {setNutrients("Ac grasos mono-in", e.target.value.toString(), 0)}}  value={micros["Ac grasos mono-in"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos mono-in" id="real_acGrasosMono" name="real_acGrasosMono" required onChange={(e) => {setNutrients("Ac grasos mono-in", e.target.value.toString(), 0)}}  value={micros["Ac grasos mono-in"]}/> <br/>
                             </div>
 
                             <div className={styles.micros_col}>
                                 <label htmlFor="real_acGrasosPoli">Ac grasos poli (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos poli" id="real_acGrasosPoli" name="real_acGrasosPoli" required onChange={(e) => {setNutrients("Ac grasos poli", e.target.value.toString(), 0)}} value={micros["Ac grasos poli"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos poli" id="real_acGrasosPoli" name="real_acGrasosPoli" required onChange={(e) => {setNutrients("Ac grasos poli", e.target.value.toString(), 0)}} value={micros["Ac grasos poli"]}/> <br/>
 
                                 <label htmlFor="real_acGrasosSaturados">Ac grasos saturados (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos saturados" id="real_acGrasosSaturados" name="real_acGrasosSaturados" required onChange={(e) => {setNutrients("Ac grasos saturados", e.target.value.toString(), 0)}}  value={micros["Ac grasos saturados"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Ac grasos saturados" id="real_acGrasosSaturados" name="real_acGrasosSaturados" required onChange={(e) => {setNutrients("Ac grasos saturados", e.target.value.toString(), 0)}}  value={micros["Ac grasos saturados"]}/> <br/>
 
                                 <label htmlFor="real_colesterol">Colesterol (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Colesterol" id="real_colesterol" name="real_colesterol" required onChange={(e) => {setNutrients("Colesterol", e.target.value.toString(), 0)}} value={micros["Colesterol"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Colesterol" id="real_colesterol" name="real_colesterol" required onChange={(e) => {setNutrients("Colesterol", e.target.value.toString(), 0)}} value={micros["Colesterol"]}/> <br/>
                                 
                                 <label htmlFor="real_potasio">Potasio (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Potasio" id="real_potasio" name="real_potasio" required onChange={(e) => {setNutrients("Potasio", e.target.value.toString(), 0)}} value={micros["Potasio"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Potasio" id="real_potasio" name="real_potasio" required onChange={(e) => {setNutrients("Potasio", e.target.value.toString(), 0)}} value={micros["Potasio"]}/> <br/>
 
                                 <label htmlFor="real_sodio">Sodio (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Sodio" id="real_sodio" name="real_sodio" required onChange={(e) => {setNutrients("Sodio", e.target.value.toString(), 0)}} value={micros["Sodio"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Sodio" id="real_sodio" name="real_sodio" required onChange={(e) => {setNutrients("Sodio", e.target.value.toString(), 0)}} value={micros["Sodio"]}/> <br/>
 
                                 <label htmlFor="real_zinc">Zinc (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Zinc" id="real_zinc" name="real_zinc" required onChange={(e) => {setNutrients("Zinc", e.target.value.toString(), 0)}} value={micros["Zinc"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Zinc" id="real_zinc" name="real_zinc" required onChange={(e) => {setNutrients("Zinc", e.target.value.toString(), 0)}} value={micros["Zinc"]}/> <br/>
 
                                 <label htmlFor="real_magnesio">Magnesio (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Magnesio" id="real_magnesio" name="real_magnesio" required onChange={(e) => {setNutrients("Magnesio", e.target.value.toString(), 0)}} value={micros["Magnesio"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Magnesio" id="real_magnesio" name="real_magnesio" required onChange={(e) => {setNutrients("Magnesio", e.target.value.toString(), 0)}} value={micros["Magnesio"]}/> <br/>
 
                                 <label htmlFor="real_vitaminaB6">Vitamina B6 (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Vitamina B6" id="real_vitaminaB6" name="real_vitaminaB6" required onChange={(e) => {setNutrients("Vitamina B6", e.target.value.toString(), 0)}} value={micros["Vitamina B6"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Vitamina B6" id="real_vitaminaB6" name="real_vitaminaB6" required onChange={(e) => {setNutrients("Vitamina B6", e.target.value.toString(), 0)}} value={micros["Vitamina B6"]}/> <br/>
                                 
                                 <label htmlFor="real_vitaminaB12">Vitamina B12 (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="VitaminaB12" id="real_vitaminaB12" name="real_vitaminaB12" required onChange={(e) => {setNutrients("Vitamina B12", e.target.value.toString(), 0)}} value={micros["Vitamina B12"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="VitaminaB12" id="real_vitaminaB12" name="real_vitaminaB12" required onChange={(e) => {setNutrients("Vitamina B12", e.target.value.toString(), 0)}} value={micros["Vitamina B12"]}/> <br/>
 
                                 <label htmlFor="real_acFolico">Ácido fólico (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Ácido fólico" id="real_acFolico" name="real_acFolico" required onChange={(e) => {setNutrients("Ácido fólico", e.target.value.toString(), 0)}} value={micros["Ácido fólico"][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Ácido fólico" id="real_acFolico" name="real_acFolico" required onChange={(e) => {setNutrients("Ácido fólico", e.target.value.toString(), 0)}} value={micros["Ácido fólico"]}/> <br/>
                                 
                                 <label htmlFor="real_folato">Folato Eq. (μg)</label> <br/>
-                                <input type="number" min="0" step="0.001"  placeholder="Folato eq" id="real_folato" name="real_folato" required onChange={(e) => {setNutrients("Folato Eq.", e.target.value.toString(), 0)}} value={micros["Folato Eq."][0]}/> <br/>
+                                <input type="number" min="0" step="0.001"  placeholder="Folato eq" id="real_folato" name="real_folato" required onChange={(e) => {setNutrients("Folato Eq.", e.target.value.toString(), 0)}} value={micros["Folato Eq."]}/> <br/>
                             </div>
                         </div>
                     </div>
