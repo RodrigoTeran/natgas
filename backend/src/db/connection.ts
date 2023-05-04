cf1c03268b68eb35d268e9d41cc791cbf67b2bc2
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-05-03 20:20:38 -0600

    fix: remove port db

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -4,8 +4,7 @@ const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: 6002
+	password: process.env.DB_PASSWORD
 });
 
 export default pool.promise();
